29b420eb356cc2f958a6bfdb994c0e6b5cca2026
richnadeau <rich.nadeau@mymail.champlain.edu>
2022-03-06 21:45:29 -0500

    vsftpd.sh upload

---
+++ Gitea-1.12.15/files/vsftpd.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mkdir /var/ftp
+mkdir /var/ftp/pub
+echo “Hello Arwen, here is your username and password. Make sure to change them after the first login” > /var/ftp/pub/backup.txt
+echo “ “ >> /var/ftp/pub/backup.txt
+echo “username: arwen” >> /var/ftp/pub/backup.txt
+echo “password: Passw0rd%231” >> /var/ftp/pub/backup.txt
