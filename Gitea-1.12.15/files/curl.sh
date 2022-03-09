f01b60b1c25b19a01e3522834e6d6e6499827e67
richnadeau <rich.nadeau@mymail.champlain.edu>
2022-03-09 18:18:06 -0500

    Gitea deployment working playbook/inventory minus passwords and flags

--- Gitea-1.12.15/files/curl.sh
+++ Gitea-1.12.15/files/curl.sh
@@ -11,7 +11,7 @@ sleep 10
 curl -i -s -k -X $'POST' \
     -H $'Host: 127.0.0.1:3000' -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0' -H $'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H $'Accept-Language: en-US,en;q=0.5' -H $'Accept-Encoding: gzip, deflate' -H $'Content-Type: application/x-www-form-urlencoded' -H $'Content-Length: 146' -H $'Origin: null' -H $'Connection: close' -H $'Upgrade-Insecure-Requests: 1' \
     -b $'lang=en-US; i_like_gitea=d093ded60f53aa16; _csrf=Iml6g5XtGqsV73_52hOYWLxuOeI6MTY0NjI2Mjg5NjYzNDM4NDMzNQ; redirect_to=%2Fuser%2Fsign_up' \
-    --data-binary $'_csrf=Iml6g5XtGqsV73_52hOYWLxuOeI6MTY0NjI2Mjg5NjYzNDM4NDMzNQ&user_name=arwen&email=arwen%40localhost.com&password=Passw0rd%231&retype=Passw0rd%231' \
+    --data-binary $'_csrf=Iml6g5XtGqsV73_52hOYWLxuOeI6MTY0NjI2Mjg5NjYzNDM4NDMzNQ&user_name=arwen&email=arwen%40localhost.com&password=Passw0rd#123&retype=Passw0rd#123' \
     $'http://127.0.0.1:3000/user/sign_up'
 
 sleep 10
--- Gitea-1.12.15/files/vsftpd.sh
+++ Gitea-1.12.15/files/vsftpd.sh
@@ -5,4 +5,4 @@ mkdir /var/ftp/pub
 echo “Hello Arwen, here is your username and password. Make sure to change them after the first login” > /var/ftp/pub/backup.txt
 echo “ “ >> /var/ftp/pub/backup.txt
 echo “username: arwen” >> /var/ftp/pub/backup.txt
-echo “password: Passw0rd%231” >> /var/ftp/pub/backup.txt
+echo “password: Passw0rd#123” >> /var/ftp/pub/backup.txt
